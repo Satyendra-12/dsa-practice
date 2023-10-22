63ac2ef7790f06f02b65f13fe0600d0fdd9efd3d
Satyendra Kumar <satyendrakumar8672461@gmail.com>
2023-10-23 01:14:44 +0530

    insertion sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,33 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-void insertion_sort(int arr[], int n){
-    for(int i=1;i<n;i++){
-        int temp =arr[i];
-        int j=i-1;
-        for(;j>=0;j--){
-          if(arr[j]>temp){
-            arr[j+1]=arr[j];
-          }
-          else{
-            break;
-          }
+
+void insertion_sort(int arr[], int n) {
+    int i = 1;
+    while (i < n) {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
         }
-        arr[j+1]=temp;
+        arr[j + 1] = temp;
+        i++;
     }
 }
-void  print(int arr[],int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";    
-         }
+
+void print(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 }
 
-int main(){
-    int arr[] = {3,2,6,4,5,1};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    insertion_sort(arr,n);
-    print(arr,n);
+int main() {
+    int arr[] = {3, 2, 6, 4, 5, 1};
+    int n = sizeof(arr) / sizeof(arr[0);
+    insertion_sort(arr, n);
+    print(arr, n);
     return 0;
 }
-
-
-// o(n)
